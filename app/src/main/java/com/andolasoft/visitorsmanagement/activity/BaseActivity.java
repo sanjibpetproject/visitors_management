3d1c62706452876f94b10708ad1aef192ca36dea
sanjib <sanjib333kumar@gmail.com>
2019-11-15 15:43:19 +0530

    Added firenase and gradle file

--- app/src/main/java/com/andolasoft/visitorsmanagement/activity/BaseActivity.java
+++ app/src/main/java/com/andolasoft/visitorsmanagement/activity/BaseActivity.java
@@ -1,14 +1,16 @@
-package com.andolasoft.visitorsmanagement;
+package com.andolasoft.visitorsmanagement.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.andolasoft.visitorsmanagement.R;
+
+public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_base);
     }
 }
