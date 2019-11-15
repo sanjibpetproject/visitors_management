8cae5ce14844dbb96d143fd42548353404476af8
sanjib <sanjib333kumar@gmail.com>
2019-11-15 15:52:45 +0530

    Added luncher screen

--- app/src/main/java/com/andolasoft/visitorsmanagement/activity/LuncherActivity.java
+++ app/src/main/java/com/andolasoft/visitorsmanagement/activity/LuncherActivity.java
@@ -2,7 +2,9 @@ package com.andolasoft.visitorsmanagement.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.andolasoft.visitorsmanagement.R;
 
@@ -12,5 +14,17 @@ public class LuncherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_luncher);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(LuncherActivity.this,BaseActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
     }
 }
